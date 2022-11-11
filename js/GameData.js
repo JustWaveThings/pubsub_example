9e0f16007b92ffdbb4b7c38ef88c8c9150d35b5e
JustWaveThings <justwavethings@proton.me>
2022-11-11 13:02:04 -0500

    Fix undefined xp variable

--- js/GameData.js
+++ js/GameData.js
@@ -5,5 +5,22 @@ new (class GameData {
 		this.xp = 0;
 		this.level = 1;
 		this.next_level_xp = 100;
+
+		setInterval(() => {
+			this.autoIncrementXp();
+		}, 1000);
+	}
+
+	addXp(amount) {
+		this.xp += amount;
+
+		PubSub.publish('xp_changed', {
+			xp: this.xp,
+			next_level_xp: this.next_level_xp,
+		});
+	}
+
+	autoIncrementXp() {
+		this.addXp(this.level * 5);
 	}
 })();
--- js/GameUi.js
+++ js/GameUi.js
@@ -5,6 +5,21 @@ new (class GameUi {
 		this.currentXp = document.getElementById('current-xp');
 		this.nextLevelXp = document.getElementById('next-level-xp');
 		this.currentLevel = document.getElementById('current-level');
+		this.currentXp = document.getElementById('current-xp');
 		this.huzzah = document.getElementById('huzzah');
+
+		PubSub.subscribe('xp_changed', (tag, data) => {
+			this.updateProgressBar(data.xp, data.next_level_xp);
+			this.updateCurrentXp(data.xp);
+		});
+	}
+
+	updateCurrentXp(xp) {
+		this.currentXp.textContent = xp;
+		console.log(xp);
+	}
+
+	updateProgressBar(xp, next_level_xp) {
+		this.progressBar.style.width = (xp / next_level_xp) * 100 + '%';
 	}
 })();
