2abbd907ef9ea9656966c0a25218a3d96f751451
JustWaveThings <justwavethings@proton.me>
2022-11-11 12:07:25 -0500

    Create GameUi class

---
+++ js/GameUi.js
@@ -0,0 +1,10 @@
+new (class GameUi {
+	constructor() {
+		this.button = document.getElementById('add-xp');
+		this.progressBar = document.getElementById('level-progress');
+		this.currentXp = document.getElementById('current-xp');
+		this.nextLevelXp = document.getElementById('next-level-xp');
+		this.currentLevel = document.getElementById('current-level');
+		this.huzzah = document.getElementById('huzzah');
+	}
+})();
