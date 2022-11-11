21e2db92a845a389948d3a6d408e2342329be312
JustWaveThings <justwavethings@proton.me>
2022-11-11 16:54:36 -0500

    Add click listener to GameUi and pub sub'd it

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # pubsub_example
- Example follow along to learn pubsubjs
 
+ Example follow along to learn pubsubjs
 
 <https://www.youtube.com/watch?v=KW5TfyxYfYw>
--- js/GameUi.js
+++ js/GameUi.js
@@ -5,13 +5,29 @@ new (class GameUi {
 		this.currentXp = document.getElementById('current-xp');
 		this.nextLevelXp = document.getElementById('next-level-xp');
 		this.currentLevel = document.getElementById('current-level');
-		this.currentXp = document.getElementById('current-xp');
 		this.huzzah = document.getElementById('huzzah');
 
 		PubSub.subscribe('xp_changed', (tag, data) => {
 			this.updateProgressBar(data.xp, data.next_level_xp);
 			this.updateCurrentXp(data.xp);
 		});
+
+		PubSub.subscribe('level_up', (tag, data) => {
+			this.congratulations(data.level, data.next_level_xp);
+		});
+
+		this.button.addEventListener('click', () => {
+			PubSub.publish('user_click_xp');
+		});
+	}
+	congratulations(level, next_level_xp) {
+		this.currentLevel.textContent = level;
+		this.nextLevelXp.textContent = next_level_xp;
+
+		this.huzzah.style.opacity = 1;
+		setTimeout(() => {
+			this.huzzah.style.opacity = 0;
+		}, 3000);
 	}
 
 	updateCurrentXp(xp) {
